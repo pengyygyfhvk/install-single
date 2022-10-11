33e73f6a254530e0a2c4dd30d56eaafb04041156
xinoso <43671653+reading8@users.noreply.github.com>
2022-10-11 20:48:37 +0800

    Update install-single.sh

--- install-single.sh
+++ install-single.sh
@@ -157,14 +157,14 @@ i18n.locale: \"zh-CN\"" > $install_home/kibana-7.8.0-linux-x86_64/config/kibana.
 nohup $install_home/kibana-7.8.0-linux-x86_64/bin/kibana --allow-root &
 }
 downPackage(){
-    wget https://xy-1257362739.cos.ap-nanjing.myqcloud.com/package/docker-20.10.8.tgz
-    wget https://xy-1257362739.cos.ap-nanjing.myqcloud.com/package/es.tar.gz
-    wget https://xy-1257362739.cos.ap-nanjing.myqcloud.com/package/filebeat-7.8.0-linux-x86_64.tar.gz
-    wget https://xy-1257362739.cos.ap-nanjing.myqcloud.com/package/redis.conf
-    wget https://xy-1257362739.cos.ap-nanjing.myqcloud.com/package/zookeeper%3Alatest
-    wget https://xy-1257362739.cos.ap-nanjing.myqcloud.com/package/openjdk-17.0.2_linux-x64_bin.tar.gz
-    wget https://xy-1257362739.cos.ap-nanjing.myqcloud.com/package/kafka%3A2.12-2.3.1
-    wget https://xy-1257362739.cos.ap-nanjing.myqcloud.com/package/kibana-7.8.0-linux-x86_64.tar.gz
+     wget https://xy-1257362739.cos.ap-nanjing.myqcloud.com/package/docker-20.10.8.tgz -P ./package/
+    wget https://xy-1257362739.cos.ap-nanjing.myqcloud.com/package/es.tar.gz ./package/
+    wget https://xy-1257362739.cos.ap-nanjing.myqcloud.com/package/filebeat-7.8.0-linux-x86_64.tar.gz ./package/
+    wget https://xy-1257362739.cos.ap-nanjing.myqcloud.com/package/redis.conf ./package/
+    wget https://xy-1257362739.cos.ap-nanjing.myqcloud.com/package/zookeeper%3Alatest ./package/
+    wget https://xy-1257362739.cos.ap-nanjing.myqcloud.com/package/openjdk-17.0.2_linux-x64_bin.tar.gz ./package/
+    wget https://xy-1257362739.cos.ap-nanjing.myqcloud.com/package/kafka%3A2.12-2.3.1 ./package/
+    wget https://xy-1257362739.cos.ap-nanjing.myqcloud.com/package/kibana-7.8.0-linux-x86_64.tar.gz ./package/
 }
 #安装docker
 #installDocker
